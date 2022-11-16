44e88d66beb4f95cc6f926740f85e99479c7994d
Rakesh Kumar Sahoo <99863834+Iamrakeshofficial@users.noreply.github.com>
2022-11-16 22:19:24 +0530

    Update settings.py

--- OjasBlog/settings.py
+++ OjasBlog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wlks7i&+h076((+x6wmo7zv83z-v55lkqyg^zwtzyy5qzwknum'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['blogrsss.herokuapp.com','127.0.0.1']
  
